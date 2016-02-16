7d72b4b9d553a40429fd242dd5099c582a2ae1dc
HimbeersaftLP <elias.nestl@outlook.de>
2016-02-16 08:39:44 +0100

    fix classpath

---
+++ src/steak/main.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace himbeersaftlp\steak;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase implements Listener{
+     public function onEnable(){
+          $this->getServer()->getPluginManager()->registerEvents($this,$this);
+          $this->getLogger()->info("Steak aktiviert!");
+     }
+     
+     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+		switch($command->getName()){
+		     
+          case "steak":
+               
+          if(!$sender instanceof Player){
+               $sender->sendMessage("Dieser Befehl geht nur INGAME!");
+          }else{
+               $sender->getInventory()->addItem(Item::get(364,0,10));
+               $sender->sendMessage("Du hast 10 gratis Steak bekommen!");
+          }
+      }
+          return true;
+      default:
+		return false;
+}
