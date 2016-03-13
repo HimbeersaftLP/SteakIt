efab72dde705951c5311d19ce5ca496de457c987
HimbeersaftLP <elias.nestl@outlook.de>
2016-03-13 15:00:39 +0100

    Update main.php

--- src/himbeer/steak/main.php
+++ src/himbeer/steak/main.php
@@ -11,20 +11,20 @@ use pocketmine\command\CommandSender;
 
 class Main extends PluginBase implements Listener{
      public function onEnable(){
-     	$this->getServer()->getPluginManager()->registerEvents($this,$this);
-     	$this->getLogger()->info("SteakIt aktiviert!");
+          $this->getServer()->getPluginManager()->registerEvents($this,$this);
+          $this->getLogger()->info("SteakIt aktiviert!");
      }
      
      public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-     	switch($command->getName()){
-     		case "steak":
-     			if(!$sender instanceof Player){
-     				$sender->sendMessage("Dieser Befehl geht nur INGAME!");
-     			}else{
-     				$sender->getInventory()->addItem(Item::get(364,0,10));
-     				$sender->sendMessage("Du hast 10 gratis Steak bekommen!");
+          switch($command->getName()){
+               case "steak":
+                    if(!$sender instanceof Player){
+                         $sender->sendMessage("Die Konsole ist leider nicht hungrig!");
+                    }else{
+                         $sender->getInventory()->addItem(Item::get(364,0,10));
+                         $sender->sendMessage("Du hast 10 gratis Steak bekommen!");
+                    }
+                    return true;
           }
-      		}
-      				return true;
      }
 }
