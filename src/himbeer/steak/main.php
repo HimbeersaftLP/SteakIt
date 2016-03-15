b0b7157a0280c39a9ffaa759f310906d53b22dcb
HimbeersaftLP <elias.nestl@outlook.de>
2016-03-15 16:53:01 +0100

    Update main.php

--- src/himbeer/steak/main.php
+++ src/himbeer/steak/main.php
@@ -18,7 +18,7 @@ class Main extends PluginBase implements Listener{
           @mkdir($this->getDataFolder());
           $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array());
           $this->config->set("steaks");
-          §amount = $this->config->get("steaks");;
+          §steaks = $this->config->get("steaks");
      }
      
      public function onCommand(CommandSender $sender, Command $command, $label, array $args){
@@ -27,7 +27,7 @@ class Main extends PluginBase implements Listener{
                if(!$sender instanceof Player){
                     $sender->sendMessage("Die Konsole ist leider nicht hungrig!");
                }else{
-                    $sender->getInventory()->addItem(Item::get(364,0,§amount);
+                    $sender->getInventory()->addItem(Item::get(364,0,§steaks);
                     $sender->sendMessage("Du hast 10 gratis Steak bekommen!");
                }
           }
