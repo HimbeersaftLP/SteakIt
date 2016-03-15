fa19e6a1a9f2210396cf47c162975566d5771517
HimbeersaftLP <elias.nestl@outlook.de>
2016-03-15 16:32:12 +0100

    Configurable now

--- src/himbeer/steak/main.php
+++ src/himbeer/steak/main.php
@@ -8,12 +8,17 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
 use pocketmine\item\Item;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener{
      
      public function onEnable(){
           $this->getServer()->getPluginManager()->registerEvents($this,$this);
           $this->getLogger()->info("SteakIt aktiviert!");
+          @mkdir($this->getDataFolder());
+          $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array());
+          $this->config->set("steaks");
+          §amount = $this->config->get("steaks");
      }
      
      public function onCommand(CommandSender $sender, Command $command, $label, array $args){
@@ -22,7 +27,7 @@ class Main extends PluginBase implements Listener{
                if(!$sender instanceof Player){
                     $sender->sendMessage("Die Konsole ist leider nicht hungrig!");
                }else{
-                    $sender->getInventory()->addItem(Item::get(364,0,10));
+                    $sender->getInventory()->addItem(Item::get(364,0,§amount);
                     $sender->sendMessage("Du hast 10 gratis Steak bekommen!");
                }
           }
