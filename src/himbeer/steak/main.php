5f90832c50c3f5dd78196bcecfc5b6ccf69003a4
HimbeersaftLP <elias.nestl@outlook.de>
2016-03-17 17:53:26 +0100

    Update main.php

--- src/himbeer/steak/main.php
+++ src/himbeer/steak/main.php
@@ -16,8 +16,20 @@ class Main extends PluginBase implements Listener{
           $this->getServer()->getPluginManager()->registerEvents($this,$this);
           $this->getLogger()->info("SteakIt enabled!");
           @mkdir($this->getDataFolder());
-          $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array());
-          $this->config->set("steaks");
+          $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array(
+
+               #     _____ _             _    _____ _   
+               #    / ____| |           | |  |_   _| |  
+               #   | (___ | |_ ___  __ _| | __ | | | |_ 
+               #    \___ \| __/ _ \/ _` | |/ / | | | __|
+               #    ____) | ||  __/ (_| |   < _| |_| |_ 
+               #   |_____/ \__\___|\__,_|_|\_\_____|\__|
+               # SteakIt configuration file:
+               # 
+               # Number of steaks to give to a player:
+               steaks: 10
+          ));
+          $this->saveResource("config.yml");
      }
      
      public function onCommand(CommandSender $sender, Command $command, $label, array $args){
@@ -28,7 +40,7 @@ class Main extends PluginBase implements Listener{
                }else{
                     $steaks = $this->config->get("steaks");
                     $sender->getInventory()->addItem(Item::get(364,0,$steaks));
-                    $sender->sendMessage("You got some free steaks!");
+                    $sender->sendMessage("You got ",§steaks," free steaks!");
                }
           }
           return true;
