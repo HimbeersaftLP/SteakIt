6e99c565c805d8f52b492a3dc259822eedafcabd
HimbeersaftLP <elias.nestl@outlook.de>
2016-07-02 14:13:09 +0200

    Rewrite Part 3/3

--- src/himbeer/steak/main.php
+++ src/himbeer/steak/main.php
@@ -1,5 +1,19 @@
 <?php
 
+/*
+ *     $$$$$$\    $$\                         $$\       $$$$$$\ $$\
+ *    $$  __$$\   $$ |                        $$ |      \_$$  _|$$ |
+ *    $$ /  \__|$$$$$$\    $$$$$$\   $$$$$$\  $$ |  $$\   $$ |$$$$$$\
+ *    \$$$$$$\  \_$$  _|  $$  __$$\  \____$$\ $$ | $$  |  $$ |\_$$  _|
+ *     \____$$\   $$ |    $$$$$$$$ | $$$$$$$ |$$$$$$  /   $$ |  $$ |
+ *    $$\   $$ |  $$ |$$\ $$   ____|$$  __$$ |$$  _$$<    $$ |  $$ |$$\
+ *    \$$$$$$  |  \$$$$  |\$$$$$$$\ \$$$$$$$ |$$ | \$$\ $$$$$$\ \$$$$  |
+ *     \______/    \____/  \_______| \_______|\__|  \__|\______| \____/
+ * 
+ * by HimbeersaftLP
+ * 
+ */
+
 namespace himbeer\steak;
 
 use pocketmine\plugin\PluginBase;
@@ -14,45 +28,72 @@ class Main extends PluginBase implements Listener{
      
      public function onEnable(){
           $this->getServer()->getPluginManager()->registerEvents($this,$this);
-          $this->getLogger()->info("SteakIt enabled!");
-          @mkdir($this->getDataFolder());
-          $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array(
-               "steaks" => 10,
-               "steak_msg" => "You got 10 free steak!",
-               "hunger_fill_msg" => "Your hunger bar had been filled!",
-               "hunger_and_steak_msg" => "You got 10 free steaks and your hunger bar had been filled!",
-               "console_msg" => "The console isn't hunry!",
-               "give_steaks" => true,
-               "fill_hunger" => true,
-          ));
+          $this->getLogger()->info("SteakIt by HimbeersaftLP enabled!");
           $this->saveResource("config.yml");
+          $this->config = new Config($this->getDataFolder(). "config.yml", Config::YAML);
      }
-     
+
+     public function giveSteak($player){
+          $count = $this->config->get("steak_count");
+          $player->getInventory()->addItem(Item::get(364,0,$count));
+          return true;
+     }
+
+     public function fillHunger($player){
+          $player->setFood(20);
+          return true;
+     }
+
      public function onCommand(CommandSender $sender, Command $command, $label, array $args){
           switch($command->getName()){
                case "steak":
                     if(!$sender instanceof Player){
-                         $console_msg = $this->config->get("console_msg");
-                         $sender->sendMessage($console_msg);
+                         $msg = $this->config->get("console_msg");
+                         $sender->sendMessage($msg);
+                         break;
                     }else{
-                         if($this->config->get("give_steaks") == true && $this->config->get("fill_hunger") == false){
-                              $steaks = $this->config->get("steaks");
-                              $sender->getInventory()->addItem(Item::get(364,0,$steaks));
-                              $steak_msg = $this->config->get("steak_msg");
-                              $sender->sendMessage($steak_msg);
-                         }elseif($this->config->get("give_steaks") == false && $this->config->get("fill_hunger") == true){
-                              $hunger_fill_msg = $this->config->get("hunger_fill_msg");
-                              $sender->sendMessage($hunger_fill_msg);
-                              $sender->setFood(20);
-                         }elseif($this->config->get("give_steaks") == true && $this->config->get("fill_hunger") == true){
-                              $hunger_and_steak_msg = $this->config->get("hunger_and_steak_msg");
-                              $steaks = $this->config->get("steaks");
-                              $sender->getInventory()->addItem(Item::get(364,0,$steaks));
-                              $sender->setFood(20);
-                              $sender->sendMessage($hunger_and_steak_msg);
+                         if(!isset($args[0])){
+                              $gsteak = $this->config->get("give_steak");
+                              $fhunger = $this->config->get("fill_hunger");
+                              if($gsteak == true){
+                                   $this->giveSteak($sender);
+                              }
+                              if($fhunger == true){
+                                   $this->fillHunger($sender);
+                              }
+                              if($gsteak == false && $fhunger == false){
+                                   $sender->sendMessage("Please set fill_hunger or give_steak in config to true!");
+                                   break;
+                              }
+                              $msg = $this->config->get("maincmd_msg");
+                              $sender->sendMessage($msg);
+                              break;
                          }else{
-                              $sender->sendMessage("Please set fill_hunger or give_steaks in config to true!");
+                              switch($args[0]){
+                                   case "steak":
+                                        $this->giveSteak($sender);
+                                        $msg = $this->config->get("subcmd_steak");
+                                        $sender->sendMessage($msg);
+                                        break;
+                                   case "hunger":
+                                        $this->fillHunger($sender);
+                                        $msg = $this->config->get("subcmd_hunger");
+                                        $sender->sendMessage($msg);
+                                        break;
+                                   case "both":
+                                        $this->giveSteak($sender);
+                                        $this->fillHunger($sender);
+                                        $msg = $this->config->get("subcmd_both");
+                                        $sender->sendMessage($msg);
+                                        break;
+                                   default:
+                                        $msg = $this->config->get("subcmd_not_found");
+                                        $sender->sendMessage($msg);
+                                        break;
+                              }
+                              break;
                          }
+                         break;
                     }
           }
           return true;
